1c6ac017feb150b7222edae4c7feb6909ac92b72
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-05-09 18:58:36 +0600

    js file added

---
+++ rgb.js
@@ -0,0 +1,14 @@
+window.onload =()=>{
+    main();
+}
+
+function main(){
+    const rgb= document.getElementById('rgb')
+    const btn=document.getElementById('btn')
+}
+
+function generatorRGBColor(){
+    const red=math.floor(math.random()*255);
+    const green=math.floor(math.random()*255);
+    const blue=math.floor(math.random()*255);
+}
